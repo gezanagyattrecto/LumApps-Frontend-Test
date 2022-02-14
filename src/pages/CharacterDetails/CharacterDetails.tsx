77f9e253b63b1ed51419b7f3c4168903e5959dab
Géza Nagy <geza.nagy@attrecto.com>
2022-02-14 14:50:54 +0100

    Handle loading states

--- src/pages/CharacterDetails/CharacterDetails.tsx
+++ src/pages/CharacterDetails/CharacterDetails.tsx
@@ -10,46 +10,56 @@ import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import LatestComics from "./LatestComics/LatestComics";
 import classes from "./CharacterDetails.module.scss";
+import { toast } from "react-toastify";
 
 const faBackwardIcon = faArrowLeft as IconProp;
 
 const CharacterDetails: FC = () => {
   const { id } = useParams();
-
+  const [loading, setLoading] = useState<boolean>(false);
   const [data, setData] = useState<MarvelCharacterModel | null>(null);
 
   useEffect(() => {
     const getCharacterDetails = async () => {
-      setData(await characterService.get(id));
+      try {
+        setLoading(true);
+        setData(await characterService.get(id));
+      } catch (e) {
+        toast.error("Something went wrong");
+      } finally {
+        setLoading(false);
+      }
     };
 
     getCharacterDetails();
   }, [id]);
 
-  console.log("data", data);
+  return (
+    <Page loading={loading}>
+      {data?.id ? (
+        <>
+          <div className="d-flex justify-content-between">
+            <div>
+              <Button variant="link" className="mb-2">
+                <FontAwesomeIcon className="mr-2" icon={faBackwardIcon} />
+                Back to results
+              </Button>
+              <h3 className="mb-3">{data.name}</h3>
+              <p>{data.description}</p>
+            </div>
+            <CharacterThumbnail thumbnail={data?.thumbnail} />
+          </div>
 
-  return data?.id ? (
-    <Page>
-      <div className="d-flex justify-content-between">
-        <div>
-          <Button variant="link" className="mb-2">
-            <FontAwesomeIcon className="mr-2" icon={faBackwardIcon} />
-            Back to results
-          </Button>
-          <h3 className="mb-3">{data.name}</h3>
-          <p>{data.description}</p>
-        </div>
-        <CharacterThumbnail thumbnail={data?.thumbnail} />
-      </div>
-
-      <div className="d-flex justify-content-between mt-3">
-        <div className="card flex-grow-1 mr-3 p-3">
-          Here comes whatever it comes
-        </div>
-        <LatestComics className={classes.LatestComics} characterId={id} />
-      </div>
+          <div className="d-flex justify-content-between mt-3">
+            <div className="card flex-grow-1 mr-3 p-3">
+              Here comes whatever it comes
+            </div>
+            <LatestComics className={classes.LatestComics} characterId={id} />
+          </div>
+        </>
+      ) : null}
     </Page>
-  ) : null;
+  );
 };
 
 export default CharacterDetails;
